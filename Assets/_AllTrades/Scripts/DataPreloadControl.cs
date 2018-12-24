df96c8ba4306309313a5e3f209800a36f8b8fed6
AllTradesGames <alltradesgames@gmail.com>
2018-12-24 14:55:56 -0700

    Oculus Player Account Frontend Retrieval
    Created "Data Preload" Scene. Initialized Oculus Platform in "DataPreloadControl.cs". Retrieved logged in Oculus user and org scoped ID.

---
+++ Assets/_AllTrades/Scripts/DataPreloadControl.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Platform;
+
+public class DataPreloadControl : MonoBehaviour
+{
+
+    Oculus.Platform.Models.User user;
+    UInt64 orgScopedID;
+
+    void Awake()
+    {
+        try
+        {
+            Core.AsyncInitialize();
+            Entitlements.IsUserEntitledToApplication().OnComplete(CheckEntitlement);
+        }
+        catch (UnityException err)
+        {
+            Debug.LogError("Platform failed to initialize due to exception.");
+            Debug.LogException(err);
+            CheckEntitlement(null);
+        }
+
+        Users.GetLoggedInUser().OnComplete((Message msg) =>
+        {
+            if (msg.IsError)
+            {
+                Debug.LogError("Error getting logged in user.");
+            }
+            else
+            {
+                user = msg.GetUser();
+                Users.GetOrgScopedID(user.ID).OnComplete((Message ms) =>
+                {
+                    if (ms.IsError)
+                    {
+                        Debug.LogError("Error getting org scoped ID.");
+                    }
+                    else
+                    {
+                        orgScopedID = ms.GetOrgScopedID().ID;
+
+                        Debug.Log("Logged in user: " + user.OculusID + ", App Relative ID: " + user.ID);
+                        Debug.Log("Org Scoped ID: " + orgScopedID);
+                    }
+                });
+            }
+        });
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CheckEntitlement(Message msg)
+    {
+        if (msg == null || msg.IsError)
+        {
+            // Entitlement check failed. Quit app
+            Debug.LogError("Oculus User Entitlement check failed.");
+            UnityEngine.Application.Quit();
+        }
+        else
+        {
+            // Entitlement check passed
+            Debug.Log("Oculus User Entitlement check passed.");
+        }
+    }
+}
